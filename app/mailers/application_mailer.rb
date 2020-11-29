facc4eaf102986bdad09d152146b6275bcda3b20
Kyle Fritz <kyle.p.fritz@gmail.com>
2020-11-29 02:36:18 -0500

    fix domain

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'no-reply@francaforthepeople.com'
+  default from: 'no-reply@tubs.fritz.io'
   layout 'mailer'
   track open: true, click: true
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "tublife_production"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = {host: "tublife.herokuapp.com"}
+  config.action_mailer.default_url_options = {host: "tubs.fritz.io"}
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
