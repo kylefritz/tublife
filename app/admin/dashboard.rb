c8c7a88b0d3d7c3281939f8aca7d0783c8bf2758
Kyle Fritz <kyle.p.fritz@gmail.com>
2020-11-29 16:51:03 -0500

    charts for tubs

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,32 +1,22 @@
 ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc { I18n.t("active_admin.dashboard") }
 
-  content title: proc { I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+  content title: "Reports" do
+    columns do
+      column do
+        def readings(device_name)
+          data = Hash[Reading.where(device_name: device_name).pluck(["created_at", "temp_f"])]
+          line_chart([{name: device_name, data: data}], min: "95", max: "108")
+        end
+
+        panel "Baltimore" do
+          readings("Kale-a Lilly")
+        end
+        panel "Richmond" do
+          readings("Hot Tub Thermometer")
+        end
       end
     end
+  end
 
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
-
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
-  end # content
 end
