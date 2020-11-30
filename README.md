72a7de66ffe98fee92780d5aabd69e4a86ae8cec
Kyle Fritz <kyle.p.fritz@gmail.com>
2020-11-29 22:49:44 -0500

    historical weather

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Tublife
 
 we're gonna push this to heroku & track the temps of the tubs
+
+```ruby
+reading_times = Reading.pluck(:created_at).map(&:beginning_of_hour).uniq
+reading_times.each do |reading_at|
+  Weather.history!("Baltimore", 39.290379, -76.61219, reading_at)
+  Weather.history!("Richmond", 37.553761, -77.460258, reading_at)
+end
+```
--- app/models/weather.rb
+++ app/models/weather.rb
@@ -2,17 +2,27 @@ API_URL = "http://api.openweathermap.org/data/2.5"
 
 class Weather < ApplicationRecord
   def self.for!(city)
-    api_key = ENV['OPEN_WEATHER_KEY']
-    if !api_key
-      throw "Missing OPEN_WEATHER_KEY from env"
-    end
-
-    raw_data = Faraday.get("#{API_URL}/weather?q=#{city},us&units=imperial&APPID=#{api_key}").body
-    weather_data = JSON.parse(raw_data, object_class: OpenStruct)
-
+    weather_data = download("weather?q=#{city},us")
     temp = weather_data.main.temp
     # weather_data.main.feels_like
 
     Weather.create!(city: city, temp_f: temp)
+  end
+
+  def self.history!(city, lat, lon, weather_at)
+    weather_data = download("onecall/timemachine?lat=#{lat}&lon=#{lon}&type=hour&dt=#{weather_at.to_i}")
+
+    temp = weather_data.current.temp
+    Weather.create!(city: city, temp_f: temp, created_at: weather_at)
+  end
+
+  def self.download(url)
+    api_key = ENV['OPEN_WEATHER_KEY']
+    throw "Missing OPEN_WEATHER_KEY from env" if !api_key
+
+    url = "#{API_URL}/#{url}&units=imperial&appid=#{api_key}"
+    puts url
+    raw_data = Faraday.get(url).body
+    JSON.parse(raw_data, object_class: OpenStruct)
   end
 end
